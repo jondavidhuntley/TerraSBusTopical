9a4b8cd4594253087c662d06510e6c256553fbdb
Jon Huntley <jon.huntley@hotmail.co.uk>
2020-01-08 14:41:13 +0000

    Added Service Bus Changes

--- main.tf
+++ main.tf
@@ -2,70 +2,30 @@ provider "azurerm" {
   version = "=1.28.0"
 }
 
-// Existing Key Vault
-data "azurerm_key_vault" "huntleywebKV" {
-  name                = var.key_vault_name
-  resource_group_name = var.hweb_resource_group_name
-}
-
-// Read Existing Key Vault Data
-data "azurerm_key_vault_secret" "HollybeckDataBaseConnectionSecret" {
-  name         = "HollybeckDatabaseConnection"
-  key_vault_id = "${data.azurerm_key_vault.huntleywebKV.id}"
-}
-
-// Create Service Plan
-resource "azurerm_app_service_plan" "service_plan" {
-  name                = var.app_service_plan_name 
+// Create ServiceBus Namespace
+resource "azurerm_servicebus_namespace" "hweb_sbus_ns" {
+  name                = "${var.prefix}-servicebus-namespace"
   location            = var.default_location
   resource_group_name = var.hweb_resource_group_name
-  kind                = "Windows"
-
-  sku {
-    tier = var.app_service_plan_tier
-    size = var.app_service_plan_size
-  }
-
+  sku                 = var.poc_sbus_ns_sku
   tags = var.tags
 }
 
-// Create Application Insights
-resource "azurerm_application_insights" "app_insights" {
-  name                = "${var.prefix}-hollybeck-api-insights"
-  location            = var.default_location
+// Create Topic
+resource "azurerm_servicebus_topic" "sbus_topic" {
+  name                = "hweb_poc_passage_published_topic"
   resource_group_name = var.hweb_resource_group_name
-  application_type    = "Web"
+  namespace_name      = "${azurerm_servicebus_namespace.hweb_sbus_ns.name}"
+
+  enable_partitioning = true
 }
 
-// Create App Service - Hollybeck API
-resource "azurerm_app_service" "app_service" {
-  name                = "${var.prefix}-hollybeck-api"
-  location            = var.default_location
-  resource_group_name = var.hweb_resource_group_name 
-  app_service_plan_id =  "${azurerm_app_service_plan.service_plan.id}"
-         
-   site_config {
-    //always_on                 = true
-    http2_enabled             = var.https_only    
-    use_32_bit_worker_process = true
-    dotnet_framework_version = "v4.0"
-  }
-
-  connection_string {
-    name  = "HollybeckDatabase"
-    type  = "SQLServer"
-    value = "${data.azurerm_key_vault_secret.HollybeckDataBaseConnectionSecret.value}"
-  }
-
-  tags = var.tags
-
-  app_settings = {
-    "APPINSIGHTS_INSTRUMENTATIONKEY" = "${azurerm_application_insights.app_insights.instrumentation_key}"
-    "WEBSITE_WEBDEPLOY_USE_SCM" = "false"
-    //"TokenServiceUri" = "https://em-si.eu.auth0.com/"
-    //"Audience" = "${var.auth0AudienceHost}taa-airline-api/"
-    "SwaggerTitle" = "Hollybeck API"
-    "SwaggerDescription" = "RESTful Service API for consumption by Associated Garden Centres"
-    "SwaggerVersion" = "v1"
-  }  
+// Create Subscriptions
+// Passage Listener
+resource "azurerm_servicebus_subscription" "sbus_sub_passage_processor" {
+  name                = "hweb_sbus_subscription_passage_processor"
+  resource_group_name = var.hweb_resource_group_name
+  namespace_name      = "${azurerm_servicebus_namespace.hweb_sbus_ns.name}"
+  topic_name          = "${azurerm_servicebus_topic.sbus_topic.name}"
+  max_delivery_count  = 1
 }
--- variables.tf
+++ variables.tf
@@ -1,19 +1,10 @@
 variable "prefix" {}
-variable "key_vault_name" {}
 variable "hweb_resource_group_name" {}
-variable "app_service_plan_name" {}
-variable "app_service_plan_tier" {}
-variable "app_service_plan_size" {}
+variable "poc_sbus_ns_sku" {}
 
 variable "default_location" {
   default = "UK South"
    description = "The default location to house everything"
-}
-
-variable "https_only" {
-  type        = bool
-  default     = false
-  description = "Redirect all traffic made to the web app using HTTP to HTTPS."
 }
 
 variable "tags" {
